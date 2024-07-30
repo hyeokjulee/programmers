a8f1a1afb48d211ecad2901dde4a15323efb761b
HyeokJu Lee <dlhjwwe@naver.com>
2024-07-30 14:10:46 +0900

    완전탐색 문제

---
+++ python/소수찾기.py
@@ -0,0 +1,22 @@
+import math
+from itertools import permutations
+
+def solution(numbers):
+    nums = set()
+    cnt = 0
+    
+    for i in range(1, len(numbers) + 1):
+        for n in permutations(numbers, i):
+            nums.add(int(''.join(n)))
+                
+    nums.discard(0)
+    nums.discard(1)
+    
+    for n in nums:
+        cnt += 1
+        for i in range(2, int(math.sqrt(n) + 1)):
+            if n % i == 0:
+                cnt -= 1
+                break
+    
+    return cnt
