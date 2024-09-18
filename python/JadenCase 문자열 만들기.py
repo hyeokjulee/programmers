cdb7d20ad2673efec43c2a7210409530eadcacb2
HyeokJu Lee <dlhjwwe@naver.com>
2024-09-18 15:20:47 +0900

    JadenCase 문자열 만들기

---
+++ python/JadenCase 문자열 만들기.py
@@ -0,0 +1,9 @@
+def solution(s):
+    s = list(s.lower())
+    
+    s[0] = s[0].upper()
+    for i in range(1, len(s)):
+        if s[i - 1] == ' ' and s[i] != ' ':
+            s[i] = s[i].upper()
+    
+    return ''.join(s)
