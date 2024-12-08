0a14fa1aaca25b3f8e47bdbd3b378c15a7c8e157
HyeokJu Lee <dlhjwwe@naver.com>
2024-12-08 18:20:21 +0900

    투포인터 문제

--- python/보석 쇼핑.py
+++ python/보석 쇼핑.py
@@ -1,24 +1,25 @@
 def solution(gems):
     set_of_gems = set(gems)
-    dic_of_new_gems = {gems[0]:1}
     l, r = 0, 0
-    answer = [0, len(gems) - 1]
+    dic_of_gems = {gems[0]:1}
+    answer = [1, len(gems)]
     
-    while r < len(gems):
-        if len(dic_of_new_gems) == len(set_of_gems):
-            if r - l < answer[1] - answer[0]:
-                answer = [l, r]
-            if dic_of_new_gems[gems[l]] > 1:
-                dic_of_new_gems[gems[l]] -= 1
-            else:
-                del dic_of_new_gems[gems[l]]
-            l += 1
-        else:
+    while True:
+        if len(set_of_gems) != len(dic_of_gems):
             r += 1
-            if r < len(gems):
-                if gems[r] in dic_of_new_gems:
-                    dic_of_new_gems[gems[r]] += 1
-                else:
-                    dic_of_new_gems[gems[r]] = 1
+            if r == len(gems):
+                break
+            elif gems[r] in dic_of_gems:
+                dic_of_gems[gems[r]] += 1
+            else:
+                dic_of_gems[gems[r]] = 1
+        else:
+            if r - l < answer[1] - answer[0]:
+                answer[0], answer[1] = l + 1, r + 1
+            if dic_of_gems[gems[l]] > 1:
+                dic_of_gems[gems[l]] -= 1
+            else:
+                del dic_of_gems[gems[l]]
+            l += 1
     
-    return [answer[0] + 1, answer[1] + 1]
+    return answer
